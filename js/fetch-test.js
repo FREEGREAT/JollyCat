d8dd9e5935cf70ca453dc16e3db6489e8e9d7839
FREEGREAT <vladdmitrenko3009@gmail.com>
2022-02-20 14:11:55 +0200

    UPD

---
+++ js/fetch-test.js
@@ -0,0 +1,23 @@
+const userURL = `https://jsonplaceholder.typicode.com/users`
+
+function sendRequest(method,url,body = null){
+    const headers = {
+        'Content-Type': 'application/json'
+    }
+    return fetch(url, {
+        method: method,
+        body: JSON.stringify(body),
+        headers: headers
+    }).then(response =>{
+        return response.json()
+    })
+
+    
+}
+const body = {
+    name: 'vovchyk',
+    age: 145
+}
+sendRequest('POST', userURL, body)
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
